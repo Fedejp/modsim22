9ae4216247738523645376ad3efee2cfef3afdf8
bizk <santiago.yanzon1999@gmail.com>
2022-09-02 17:03:01 -0300

    init

--- src/eulerPlus.js
+++ src/eulerPlus.js
@@ -5,9 +5,27 @@ export function calculateEulerPlus(
   intervalEnd,
   nPoints
 ) {
+  const t = 0;
+  const f = (x, t) => {
+    return eqDif(x, t);
+  }
+
+  let x = initialCondition;
+  let h = (intervalEnd-intervalStart)/nPoints;
+  
+  let predictor = initialCondition + h*f(x, t) // (yn+1) = Yn + h*f(xn, yn)
+  let fn_1 = initialCondition + (h/2)*(f(x, t) + f(x+1, predictor)) 
+
+  let resultArray = [];
+  for (let n = 0   ; n < nPoints; n++) {
+    let x = initialCondition + h*n;
+    let x_1 = x + h // X+1 
+  }
+
   return [
     { x: 0, y: 0 },
     { x: 1, y: 1 },
     { x: 3, y: 3 },
   ];
 }
+
