cc174209944c332a8bc509bf7b58562c5d9f64a7
Federico Parodi <federico.parodi@sovanta.com>
2022-09-12 17:08:02 -0300

    feat: add info

--- src/info.js
+++ src/info.js
@@ -1,6 +1,15 @@
-import { calculateEuler } from "./euler";
-
 export const eulerInfo = `#### Método de Euler
+  Es el método de Runge-Kutta más simple: aproxima el valor de f(x,t) en “un sólo paso”. 
+  
+  Al evaluar la función dada en su primera derivada, se obtiene el valor de la pendiente en el punto inicial. De esta forma, ese número se puede utilizar como entrada para calcular el valor de x en el resto de los puntos del intervalo de interés (a,b).
+  
+  La fómula utilizada para calcular cada punto de éste método es:
+
+  ![Formula de Euler](./../euler.png)
+
+  …siendo k el número de paso (o iteración) del método, y h, la distancia en 't' que se avanza en cada iteración.
+
+  Éste método genera, de forma iterativa, una poligonal que aproxima el resultado de la función f(x,t). A más grande valor de n, más cercano al valor real estará el resultado, pero más puede afectar la capacidad de cómputo del dispositivo utilizado en el redondeo del error al multiplicar por valores muy pequeños.
 
 #### Implementacion en JavaScript
 ~~~js
@@ -30,8 +39,17 @@ export function calculateEuler(eqDif, x0, a, b, nPoints) {
 `;
 
 export const eulerPlusInfo = `#### Método de Euler Mejorado
+  Una forma de reducir el error en la aproximación del método de Euler es introducir el concepto de un “predictor”: el método de Euler asume que el valor de la pendiente de la función en Xk es igual al de la pendiente en Xk+1, y ésto no siempre es verdad.
+  
+  Recalculando ésta en ambos puntos y promediando sus valores, se puede obtener una mejor aproximación de la pendiente de la función real en el intervalo (k, k+1). Éste es un método de Runge-Kutta de grado 2.
+  
+  Si calculamos el valor de Xk con el método de Euler y lo llamamos X0k, podemos usarlo como *predictor* en el cálculo final de Xk, usando la fórmula:
 
-#### Implementacion en JavaScript
+  ![Formula de Euler Mejorado](./../eulerPlus.png)
+
+  De esta manera, el método obtiene una poligonal que aproxima la función objetivo con menor porcentaje de error que el método de Euler simple, pero con las mismas salvedades para el tamaño de n y h.
+
+  #### Implementacion en JavaScript
 ~~~js
 import * as math from "mathjs";
 
@@ -62,8 +80,9 @@ export function calculateEulerPlus(eqDif, x0, a, b, nPoints) {
 `;
 
 export const compareInfo = `#### Comparación entre los métodos
-Euler = Verde
-Euler mejorado = Celeste
+Euler = Linea Verde
+
+Euler mejorado = Linea Celeste
 
 #### Implementacion en JavaScript (Euler)
 ~~~js
